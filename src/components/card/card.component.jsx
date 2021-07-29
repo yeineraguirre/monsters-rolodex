99ba3cc263d67493ca97afb0b752b2d685bd153d
Yeiner Aguirre Obregon <yeiner.obregon@gorillalogic.com>
2021-07-29 09:02:35 -0600

    Initial Commit

---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './card.styles.css';
+
+export const Card = (props) => (
+	<div className="card-container">
+		<img
+			alt="monster"
+			src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}
+		/>
+		<h2>{props.monster.name}</h2>
+		<p>{props.monster.phone}</p>
+		<p>{props.monster.website}</p>
+		<p>{props.monster.email}</p>
+	</div>
+);
